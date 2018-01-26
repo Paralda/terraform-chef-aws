089c71160990af93c118c704a07daa054e7b2702
Paralda <willbennett@knights.ucf.edu>
2018-01-26 11:51:25 -0500

    Added MySQL DB creation

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,5 +5,6 @@ log_level                :info
 log_location             STDOUT
 node_name                "paralda"
 client_key               "#{current_dir}/paralda.pem"
-chef_server_url          "https://chef.willbennett.test/organization/test/"
+chef_server_url          "https://chef.willbennett.tech/organizations/test/"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+ssl_verify_mode          :verify_none
